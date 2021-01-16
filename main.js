c993cdea333ed9e6d1a3134583c5be026d2275fb
sungwon Hong <bbh653@naver.com>
2021-01-16 21:03:17 +0900

    CSS: project fix

--- main.js
+++ main.js
@@ -81,6 +81,97 @@ function scrollIntoView(selector){ //scrollIntoView함수 생성 링크 광역
 
 }
 
+ 
+// /*
+//     1. 인터섹션 옵저버로 모든 섹션들의 진입과 나가는 것을 관찰하기
+//     2. 그렇게 하기 위해선 모든 섹션 요소를 받아온다
+//     3. 옵저버를 이용해서 모든섹션들을 관찰
+//     4. 관찰된 보여지는 섹션의 해당하는 메뉴 아이템을 활성화 시킨다.
+// */ 
+
+// const sectionIds = [ // 섹션의 배열들을 가져옴
+//     '#home',
+//     '#about',
+//     '#m_about',
+//     '#skills',
+//     '#project',
+//     '#contact'
+// ]
+
+// // 새로운 것으로 변환할 수 있는 API인 map를 사용
+// const sections = sectionIds.map(id => document.querySelector(id));
+// // console.log(sections); // 섹션을 잘 가져왔는지 확인
+
+// // 네비게이션 메뉴 아이템 받아오기
+// const navItems = sectionIds.map(id =>
+//     document.querySelector(`[data-link="${id}"]`)
+//   );// id를 이용해서 받아온다.
+// //console.log(navItems); 
+
+// let selectedNavIndex = 0;
+// let selectedNavItem = navItems[0];
+
+// function selectNavItem(selected) {
+//     selectedNavItem.classList.remove('active');
+//     selectedNavItem = selected; // navitems에 내비게이션 아이템의 요소들이 들어있고 인덱스를 이용해서 해당하는 nav 아이템을 가지고 왔다. 
+//     selectedNavItem.classList.add('active');
+    
+// }
+
+// /*
+// 2. 인터섹션 옵저버 구현
+// 새로운 옵저버를 만들어서 인터섹션 옵저 그리고 해당하는 콜백을 전달하고 옵션을 전달 한 다음에 만들어진 observer를 이용해서 각각의 섹션들을 관찰
+// */ 
+
+// const observerOptions = { // 옵저버 옵션 객체 생성 루트는 뷰포트이고, 루트마진은 기본, 스레시 홀드는 0.3 섹션이 조금만 나가거나 들어왔을때 바로 콜백함수가 호출 될 수 있도록
+//     root: null,
+//     rootMargin : '0px',
+//     threshold:0.3,
+// }
+// const observerCallback = (entries, observer) => { // 옵저버 콜백 생성
+//     entries.forEach(entry => {
+//         // console.log(entry.target);
+//         if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+//             //indexof 라는 배열 API를 사용해서 entry에 있는 타겟 아이디를 가져와서 인덱스를 찾는다
+//             console.log(entry);
+//             const index = sectionIds.indexOf(`#${entry.target.id}`);
+//             console.log(index, entry.target.id);
+//             // 스크롤링이 아래로 되어서 페이지가 올라옴
+//             if (entry.boundingClientRect.y < 0) {
+//                 selectedNavIndex = index + 1;
+//                 } else {
+//                 selectedNavIndex = index - 1;
+//             }
+           
+//         }
+//     });
+// }
+// // 관찰자 생성
+// const observer = new IntersectionObserver(observerCallback, observerOptions);// 옵저버 콜백과 옵저버 옵션 전달
+// sections.forEach(section => observer.observe(section)); // 관찰 요청
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 프로젝트 카테고리 버튼 클릭 시 해당 자료만 보여주기 기능
 const workBtnContainer = document.querySelector('.project__categories');
 const projectContainer = document.querySelector('.project__view');
